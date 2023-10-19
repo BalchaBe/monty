bd66a503f581dfffd1cb8f6bc0ec4bb3a897a1d3
BalchaBe <balcha36@yahoo.com>
2023-10-19 03:42:38 -0700

    monty.h

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * prints the top
+ * stack head
+ * line_number
+ * no return
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
